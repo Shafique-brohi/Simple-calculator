28dfd891fa2ec4745f6b03c995d81699eb2c7ba3
Shafique-brohi <161844016+Shafique-brohi@users.noreply.github.com>
2024-03-30 16:43:02 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import ListPrompt from "inquirer/lib/prompts/list.js";
+ 
+let answers = await inquirer.prompt([
+    
+       {message : "Enter your first number", type: "number", name : "firstNumber"},
+       {message: "Enter your second number", type: "number", name : "secondNumber"},
+       {
+        message: " select one operator to perform operations",
+        type: "list",
+        name : "operator",
+        choices :["addition", "subtraction", "multiplication", "division"],
+       }
+    ]);
+    // conditional statement
+    if(answers.operator=== "addition"){
+        console.log(answers.firstNumber + answers.secondNumber);
+    }else if(answers.operator=== "subtraction"){
+        console.log(answers.firstNumber - answers.secondNumber);
+    }else if(answers.operator=== "multiplication"){
+        console.log(answers.firstNumber * answers.secondNumber);
+    }else if(answers.operator=== "division"){
+        console.log(answers.firstNumber / answers.secondNumber);
+    };
